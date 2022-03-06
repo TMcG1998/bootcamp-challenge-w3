15dc1fdd8ae5a5f0ed35d538714732da60434510
Tommy McG <thomasjmcgovern98@gmail.com>
2022-03-06 15:13:22 -0500

    select length and decide between lower or upper case letters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,39 @@
 // Assignment code here
+// global variables 
+var passLength = 0;
+var lowercases = false;
+var uppercases = false;
+var numerics = false;
+var specialchars = false;
 
+var lengthSelect = function() {
+  var lengthprompt = window.prompt("Type your password length. It must be between 8 and 128.");
+  // Store value to check
+  var parse = parseInt(lengthprompt);
+  var intCheck = Number.isInteger(parse);
+  // Make sure the value is an integer to compare and is a valid length
+  if(intCheck && (parse >= 8) && (parse <= 128)) {
+      window.alert("Your password will be " + parse + " characters long.");
+      passLength = parse;
+          // Continue running code 
+  } else {
+      window.alert("Please enter a valid integer between 8 and 128.")
+      lengthSelect();
+  }
+}
+
+var caseSelect = function() {
+  var lowCasePrompt = window.confirm("Would you like your password to include lower case letters?");
+  lowercases = lowCasePrompt;
+  var upperCasePrompt = window.confirm("Would you like your password to include upper case letters?");
+  uppercases = upperCasePrompt;
+}
+
+var generatePassword = function() {
+  lengthSelect();
+  caseSelect();
+  console.log("Length: " + passLength + ". Lower cases: " + lowercases + ". Upper cases: " + uppercases);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
